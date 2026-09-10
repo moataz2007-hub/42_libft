1ad7628e6ef9f26de7e006516b4a1658e794bb21
Moataz Mansour <mansourmoataz70@gmail.com>
2026-09-10 14:54:51 +0300

    Create ft_memove.c

---
+++ ft_memove.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	if (d < s)
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	else
+	{
+		d += n;
+		s += n;
+		while (n--)
+		{
+			d--;
+			s--;
+			*d = *s;
+		}
+	}
+	return (dest);
+}
